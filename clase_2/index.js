3176aaf43cc1184d758e77abb282f94f38951a66
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2022-01-20 20:54:33 -0400

    ejemplos con variables, constantes y tipos de datos

---
+++ clase_2/index.js
@@ -0,0 +1,49 @@
+// Ejercicios con variables y constantes
+
+// Imprimir mensaje en la consola
+console.log("Hola mundo")
+
+// let 
+// var
+
+//let nombre = 'Un cadena de texto'
+let Nombre = 'nombre con la primera letra mayus.';
+let edad = 25
+let precio = 50.3245
+let impuesto = '18'
+let miVariable = 'a'
+
+console.log('----------------------------------')
+console.log(Nombre)
+console.log(typeof(Nombre))
+
+Nombre = 'otro nombre';
+console.log('----------------------------------')
+console.log(edad)
+console.log(typeof(edad))
+console.log('----------------------------------')
+console.log(precio)
+console.log(typeof(precio))
+
+const MI_CONSTANTE = 'jcapellan@itla.edu.do'
+
+console.log('----------------------------------')
+let resultado = precio + impuesto
+console.log(precio + impuesto)
+console.log(typeof(precio + impuesto))
+console.log(resultado)
+
+console.log('----------------------------------')
+
+// Castear/transformar a tipo de dato number
+miVariable = +miVariable
+console.log(precio + miVariable)
+
+// Castear/transformar a tipo de dato number
+impuesto = +impuesto
+console.log(precio + impuesto)
+
+
+
+
+
